0faca7f760ddd6789ef2a1d9b43b3988e7a7ff13
Makif <makifkavukcu@gmail.com>
2022-01-09 16:08:59 +0200

    updated

--- src/test/java/com/AMD/runners/CukesRunner.java
+++ src/test/java/com/AMD/runners/CukesRunner.java
@@ -8,14 +8,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html:target/default-cucumber-reports",
-                "json:target//json-reports/cucumber.json"
+                "pretty"
         },
         features = "src/test/resources/features",
         glue = "com/AMD/stepDefinitions",
         dryRun = false
-//        tags = "@api
-
 
 )
 public class CukesRunner {
